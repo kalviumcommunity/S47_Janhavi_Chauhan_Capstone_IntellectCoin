b51c9af3d341824196198cdc7e6d6be860b79a49
janhavichauhan <chauhanjanhavi45@gmail.com>
2024-03-15 15:14:42 +0530

    Login and Signup UI made

--- Client/Client/src/App.jsx
+++ Client/Client/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import RegistrationForm from './components/About';
 import Educations from './components/Education';
+import LoginPage from './components/Login';
+import SignupPage from './components/Signup';
 
 function App() {
   return (
@@ -16,6 +18,10 @@ function App() {
               <Link to="/registration"><button>About</button></Link>
               <br />
               <Link to="/educations"><button>Educations</button></Link>
+              <br />
+              <Link to="/login"><button>Login</button></Link>
+              <br />
+              <Link to="/signup"><button>Signup</button></Link>
             </li>
           </ul>
         </nav>
@@ -24,6 +30,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/registration" element={<RegistrationForm />} />
           <Route path="/educations" element={<Educations />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
         </Routes>
       </Router>
     </div>
