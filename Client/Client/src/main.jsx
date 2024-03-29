fbba2c913861e091f9f33e42cc62d9070b349a38
janhavichauhan <chauhanjanhavi45@gmail.com>
2024-03-29 16:41:50 +0530

    Added 3rd party authentication by auth0

--- Client/Client/src/main.jsx
+++ Client/Client/src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
+    <Auth0Provider
+    domain="dev-qpwxwlzkanw3hd2k.us.auth0.com"
+    clientId="JMch1qazOESQWEE37jPPcvG4IKxx0bw6"
+    authorizationParams={{
+    redirect_uri: window.location.origin
+    }}
+    >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>
+  </>
+    
+
 )
