0021438de912435a5ecffc3aa9d8ec85599c87f8
janhavichauhan <chauhanjanhavi45@gmail.com>
2024-04-24 10:35:31 +0530

    Made changes in folder on client

--- Client/src/components/Blog.jsx
+++ Client/src/components/Blog.jsx
@@ -4,7 +4,7 @@ const Blog = () => {
   const [posts, setPosts] = useState([
     { id: 1, title: 'First Post', content: 'This is the content of the first post.' },
     { id: 2, title: 'Second Post', content: 'This is the content of the second post.' },
-
+ls
   ]);
 
   const [currentPost, setCurrentPost] = useState(null);
