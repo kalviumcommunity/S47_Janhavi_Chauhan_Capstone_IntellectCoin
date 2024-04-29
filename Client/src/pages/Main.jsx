8bc65f5faa4a002ea792d3a1a3276e22988a25c4
janhavichauhan <chauhanjanhavi45@gmail.com>
2024-04-29 10:57:23 +0530

    Added landing page UI

--- Client/src/pages/Main.jsx
+++ Client/src/pages/Main.jsx
@@ -2,10 +2,6 @@ import React from "react";
 import { useTypewriter , Cursor } from "react-simple-typewriter";
 import styles from "./Main.module.css";
 import {Link} from 'react-router-dom'
-import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, FadeOut, Move, MoveIn, MoveOut, Sticky, StickyIn, StickyOut, Zoom, ZoomIn, ZoomOut } from "react-scroll-motion";
-const ZoomInScrollOut = batch(Sticky(), Fade(), ZoomIn());
-const FadeUp = batch(Fade(), Move(), Sticky());
-import welcome from "../assets/Welcome.png";
 import '../components/button.css'
 
 
@@ -27,6 +23,7 @@ function Main() {
   return (
     <>
        <div className={styles.main}>
+        <h3> Enrich your Learning and earning journey with Intellect Coin</h3>
        <h1 className={styles.static}>
         LEARN <span className={styles.dynamic}>{typeEffect}</span> <span className={styles.cursor}> <Cursor /></span>
       </h1>
@@ -42,40 +39,7 @@ function Main() {
 </Link>
      <Link to="/signup"><button className={styles.signup}>Signup </button></Link>
       </div>
-      {/* <ScrollContainer >
-      <ScrollPage>
-        <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -200))}>
-          <span style={{ fontSize: "30px", color: "rgb(84,121,247)"}}>Let's start our learning and earning journey!! </span>
-        </Animator>
-      </ScrollPage>
-      <ScrollPage>
-        <Animator animation={ZoomInScrollOut}>
-          <span style={{ fontSize: "40px" }}>LEARN2EARN</span>
-        </Animator>
-      </ScrollPage>
-      <ScrollPage>
-        <Animator animation={FadeUp}>
-        <img src={welcome} alt="Welcome" style={{ width: "300px" }} />
-        </Animator>
-      </ScrollPage>
-      <ScrollPage>
-        <Animator animation={ZoomInScrollOut}>
-          <span style={{ fontSize: "40px" }}>Welcome to Intellect Coin..!</span>
-        </Animator>
-      </ScrollPage>
-      <ScrollPage>
-        <Animator animation={batch(Fade(), Sticky())}>
-          <span style={{ fontSize: "40px" }}>Join our community!!</span>
-          <br/>
-          <span style={{ fontSize: "30px" }}>
-          <Link to="/login">  
-<button className={styles.login} style={{backgroundColor:"rgb(84,121,247)",color:"white", marginTop:"10px",marginRight:"10px",marginLeft:"100px"}}>Login</button>
-</Link>
-     <Link to="/signup"><button className={styles.signup}>Signup </button></Link>
-          </span>
-        </Animator>
-      </ScrollPage>
-    </ScrollContainer> */}
+     
     </>
   );
 }
